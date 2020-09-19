510f5a1da6af89c3d7b2438b3f0c76093e18fff1
Dierk Koenig <dierk.koenig@canoo.com>
2020-09-19 18:05:05 +0200

    solve chapter 2 exercises

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,11 @@
 module Test.MySolutions where
 
 import Prelude
+import Math (sqrt, pi, e)
+import Global (readFloat)
+
+diagonal anKathete gegenKathete = sqrt (anKathete * anKathete + gegenKathete * gegenKathete)
+
+circleArea radius = radius * radius * pi
+
+addE num = e + readFloat num
