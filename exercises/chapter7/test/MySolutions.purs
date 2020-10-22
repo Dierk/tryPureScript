dd1fd1ae06ac73ce9c8a4b63e2dfcabd5aa1b897
Dierk Koenig <dierk.koenig@canoo.com>
2020-10-22 17:28:41 +0200

    chapter 7 second exercises

--- exercises/chapter7/test/MySolutions.purs
+++ exercises/chapter7/test/MySolutions.purs
@@ -3,7 +3,16 @@ module Test.MySolutions where
 import Data.Maybe
 import Prelude
 
-import Control.Apply 
+import Control.Apply (lift2)
+import Data.Either (either, fromRight)
+import Data.FoldableWithIndex (foldMapWithIndexDefaultL)
+import Data.String.Regex (Regex, regex, test)
+import Data.String.Regex.Flags (noFlags)
+import Partial.Unsafe (unsafePartial)
+
+import Data.AddressBook
+import Data.AddressBook.Validation
+import Data.Validation.Semigroup 
 
 -- lift3 is well motivated. Note that 'map' also lifts.
 -- <> $ <$> <*>
@@ -33,3 +42,37 @@ combineMaybe :: forall a f. Applicative f => Maybe (f a) -> f (Maybe a)
 combineMaybe  Nothing  = pure Nothing
 combineMaybe (Just fa) = Just <$> fa
 -- combineMaybe (Just fa) = pure Just <*> fa        -- works as well
+
+-- the validation use case is cool but at the same time
+-- seems a bit overly complicated for such a simple task.
+-- Where is the striking benefit? Compositionality of validations?
+
+-- note pn.type can be written as pn."type" to avoid the reserved keyword restriction
+
+
+failingRegex :: Regex
+failingRegex = unsafePartial $ fromRight $ regex "(?!)" noFlags 
+
+--- only for literal Strings
+staticRegex :: String -> Regex
+-- staticRegex str = unsafePartial $ fromRight $ regex str noFlags                    -- neither is nice
+staticRegex str = either (const failingRegex) identity $ regex str noFlags
+
+stateRegex :: Regex
+stateRegex = staticRegex "^[a-zA-Z]{2,2}$"         -- no special escaping syntax?
+
+nonEmptyRegex :: Regex
+nonEmptyRegex = staticRegex "\\S"                  -- in other words: at least one non-space char
+
+
+validateState :: String -> V Errors String
+validateState state = matches "State" stateRegex state *> pure state
+
+validateNonEmpty :: String -> String -> V Errors String
+validateNonEmpty fieldName value = matches fieldName nonEmptyRegex value *> pure value
+
+validateAddressImproved :: Address -> V Errors Address
+validateAddressImproved a =
+  address <$> (validateNonEmpty "Street" a.street)
+          <*> (validateNonEmpty "City"   a.city  )
+          <*> (validateState a.state)
