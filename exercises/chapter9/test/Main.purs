77ec5056b548d1cc1d45db50572bbf3f4cc0ec77
Dierk Koenig <dierk.koenig@canoo.com>
2020-11-21 22:44:25 +0100

    put testCopy and testConcatenate in sequence

--- exercises/chapter9/test/Main.purs
+++ exercises/chapter9/test/Main.purs
@@ -3,10 +3,11 @@ module Test.Main where
 import Prelude
 import Test.MySolutions
 
-import Data.Either (Either(..),either)
+import Data.Either (Either(..), either)
 import Effect (Effect)
 import Effect.Aff (Aff, attempt, message, runAff_)
 import Effect.Class.Console (log)
+import Web.File.FileReader (result)
 
 
 testCopy :: Aff Unit
@@ -28,8 +29,17 @@ runAffTest affTest continuation = flip runAff_ affTest $ either
   (log <<< message) 
   (\_ -> continuation)
 
+foldM :: forall a. (Effect Unit -> Aff a -> Effect Unit) -> Unit -> Array (Aff a) -> Effect (Effect Unit)
+
+runInSequence affTests = do 
+  result <- foldM ( \effect affTest -> runAffTest affTest effect) unit affTests
+  bare   <- result
+  pure bare
+
 main :: Effect Unit
 main = do
-  runAffTest testCopy $
-    runAffTest testConcatenate $
-      log "done"
+  runInSequence [testCopy, (log "done")]
+  --runAffTest testCopy $
+    --runAffTest testConcatenate $
+      --log "done"
+
